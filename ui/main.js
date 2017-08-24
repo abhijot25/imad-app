e84cc35829c49261e46a585fc76d0a028da61acd
abhijot <simarsingh187@gmail.com>
2017-08-25 02:43:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,17 +25,3 @@ console.log('Loaded!');
        request.send(null);
    };
    
-var nameInput=document.getElementById("name").value+='';
-var submit=document.getElementById('submitb');
-submit.onclick=function()
-{
-  var names=['name1','name2','name3'];
-  var list=" ";
-  for(var i=1;i<=names.length;i++)
-  {
-      list='<li>'+names[i]+'</li>';
-      
-  }
-  var ul=document.getElementById('namelist').innerHTML=list;
-  
-};
