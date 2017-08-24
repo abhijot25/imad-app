335cf6c86b699e5b4d09d97efb0f2b585db6e241
abhijot <simarsingh187@gmail.com>
2017-08-25 01:56:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,70 +1,27 @@
 console.log('Loaded!');
-   function c7()
-	{
-		document.getElementById("txt").value+="7";
-	}
-function c8()
-	{
-		document.getElementById("txt").value+="8";
-	}
-function c9()
-	{
-		document.getElementById("txt").value+="9";
-	}
-function cP()
-	{
-		document.getElementById("txt").value+="+";
-	}
-function c4()
-	{
-		document.getElementById("txt").value+="4";
-	}
-function c5()
-	{
-		document.getElementById("txt").value+="5";
-	}
-function c6()
-	{
-		document.getElementById("txt").value+="6";
-	}
-function cM()
-	{
-		document.getElementById("txt").value+="-";
-	}
-function c1()
-	{
-		document.getElementById("txt").value+="1";
-	}
-function c2()
-	{
-		document.getElementById("txt").value+="2";
-	}
-function c3()
-	{
-		document.getElementById("txt").value+="3";
-	}
-function cMl()
-	{
-		document.getElementById("txt").value+="*";
-	}
-function cC()
-	{
-		document.getElementById("txt").value+="C";
-	}
-function cZ()
-	{
-		document.getElementById("txt").value+="0";
-	}
-function cE()
-	{
-		document.getElementById("txt").value+="=";
-	}
-function cE()
-	{
-		var a=eval(document.getElementById("txt").value);
-		document.getElementById("txt").value=a;
-	}
-function cD()
-	{
-		document.getElementById("txt").value+="/";
-	}
+   var button= document.getElementById('button');
+   
+   button.onclick=function()
+   {
+       // create request
+       var reqest =new XMlHttpRequest();
+       //capture the response and store in the variable 
+       request.onreadystatechange=function()
+       {
+            
+           if(request.readystate==XMLHttprequest.DONE){
+           //Take some action
+           if(request.status==200){
+               var counter=request.responseText;
+               var span=document.getElementById('count');
+               span.innerHTML=counter.toString();
+           }
+           }
+           //don't do anything
+       };
+       
+       //make request
+       request.open('GET', 'http://simarsingh187.imad.hasura.io/counter',true);
+       request.send(null);
+   };
+   
