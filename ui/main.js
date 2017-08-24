f0d8b675befbf644b262207768d0f2d304c87004
abhijot <simarsingh187@gmail.com>
2017-08-25 02:28:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,12 +27,12 @@ console.log('Loaded!');
    
 var nameInput=document.getElementById("name");
 var name=nameInput.value;
-var submit=document.getElementById('submit');
+var submit=document.getElementById('submitb');
 submit.onclick=function()
 {
   var names=['name 1','name2','name3'];
   var list=" ";
-  for (var i=0;i<names.length;i++)
+  for (var i=0;i<=names.length;i++)
   {
       list+="<li>"+names[i]+"</li>";
       
