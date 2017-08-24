e0c0b452123522c93ef72dbad1dd73b51e2ad194
abhijot <simarsingh187@gmail.com>
2017-08-25 02:31:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,6 +37,6 @@ submit.onclick=function()
       list+="<li>"+names[i]+"</li>";
       
   }
-  var ul=document.getElementById('namelist');
-  ul.innerHTML=list;
+  var ul=document.getElementById('namelist').innerHTML=list;
+  
 };
